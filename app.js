b4e28dfd2204517efc0792c99f2af029e76beb8a
BibianaBalBar <balbuenabibi@gmail.com>
2020-11-30 12:08:57 +0000

    media queries

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ function animateSlides() {
     });
     slideTl.fromTo(revealImg, {x: "0%"} , {x: "100%"});
     slideTl.fromTo(img, {scale: 2} , {scale: 1}, '-=1');
-    slideTl.fromTo(revealText, {x: "0%"} , {x: "100%"}, '-=0.75');
-    slideTl.fromTo(nav, {y: "-100%"} , {y: "0%"}, '-=0.5');
+    slideTl.fromTo(revealText, {x: "0%"} , {x: "100%"}, '-=0.75');    
     //Create Scene
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
@@ -124,8 +123,7 @@ barba.init({
       namespace: 'fashion',
       beforeEnter() {
         logo.href = "../index.html";
-        detailAnimation();
-        gsap.fromTo('.nav-header', 1, {y:'100%'}, {y: '0%', ease: "power2.inOut"});
+        detailAnimation();        
       },
       beforeLeave(){
         controller.destroy();
@@ -148,8 +146,19 @@ barba.init({
         let done = this.async();
         //An animation
         const tl = gsap.timeline({defaults: {ease: 'power2.inOut'}});
-        tl.fromTo('.swipe', 1, {x: '0%'}, {x: '100%', stagger: 0.25, onComplete: done});
+        tl.fromTo(
+          '.swipe',
+          1,
+          {x: '0%'},          
+          {x: '100%', stagger: 0.25, onComplete: done}
+        );
         tl.fromTo(next.container, 1, {opacity: 0}, {opacity: 1});
+        tl.fromTo(
+          '.nav-header',
+          1, 
+          {y:'-100%'}, 
+          {y: '0%', ease: "power2.inOut"}, '-=1.5'
+        );
       }
     }
   ]
